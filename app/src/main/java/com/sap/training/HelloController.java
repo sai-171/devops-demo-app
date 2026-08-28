acd97da785f847989a17cbde8e5259c00c484ea4
sai-171 <strikersairam971@gmail.com>
2026-08-28 14:12:35 +0530

    my first pipeline trigger

--- app/src/main/java/com/sap/training/HelloController.java
+++ app/src/main/java/com/sap/training/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from SAP DevOps Training!";
+        return "Hello from Sairam — DevOps is live!";
     }
 }
